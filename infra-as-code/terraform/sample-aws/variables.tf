d10f95bbf4ca81a365a4792b0b71c4a62a7de2ad
nikhilmulinti <127198713+nikhilmulinti@users.noreply.github.com>
2024-12-09 15:00:11 +0530

    updating arn

--- infra-as-code/terraform/sample-aws/variables.tf
+++ infra-as-code/terraform/sample-aws/variables.tf
@@ -73,6 +73,10 @@ variable "enable_karpenter" {
   type        = bool
   default     = false
 }
+variable "iam_user_arn" {
+  description = "Provide the IAM user arn which you are using to create infrastructure"
+  default = "<iam_user_arn>" #Example: arn:aws:iam::4332145635273:user/demo
+}
 ## change ssh key_name eg. digit-quickstart_your-name
 
 
